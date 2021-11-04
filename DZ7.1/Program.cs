3095cceab8196d49215476e11d195be6efcee597
feravn28 <92865552+feravn28@users.noreply.github.com>
2021-11-04 21:10:20 +0300

    добавлено решение на задачу №2

---
+++ DZ7.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToInt32(Console.ReadLine());
+            double V, S;
+            CoubeReb(a, out V, out S);
+            Console.WriteLine("Площадь куба:\t" + S + " кв.см");
+            Console.WriteLine("Объём куба:\t" + V + " куб.см");
+            Console.ReadKey();
+
+        }
+        static double CoubeReb(double a, out double V, out double S)
+        {
+            V = Math.Pow(a, 3);
+            S = Math.Pow(a, 2) * 6;
+            return V;
+        }
+    }
+}
